6d209c51dd0e0a4914a67217d36eb265e1a7f086
ShawSensei <shawkatwasit106@gmail.com>
2025-08-14 16:01:27 +0600

    Initial Commit

---
+++ MyWeatherTests/MyWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherTests.swift
+//  MyWeatherTests
+//
+//  Created by MacPc4 on 8/14/25.
+//
+
+import Testing
+@testable import MyWeather
+
+struct MyWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
